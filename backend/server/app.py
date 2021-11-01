61214d02cf6be6647669c7cb9696bc79716609cf
Kurt Ball <kurtisdball@hotmail.com>
2021-10-31 19:51:58 -0600

    CORS and environment variable

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.routes.user import router as UserRouter
 
 app = FastAPI()
 
+origins = [
+    f"{API_CONNECTION}",
+    "http://localhost:8000",
+]
 app.include_router(UserRouter, tags=["Users"], prefix="/api")
 
 
